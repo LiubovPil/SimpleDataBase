967f92ae3a80bb52b29787fccc865c9c77fe936f
LiubovPil <alexandraelle95@gmail.com>
2022-07-27 13:28:20 +0300

    SimpleDataBase finish

--- SimpleDataBase/SimpleDataBase/SimpleDataBase.cpp
+++ SimpleDataBase/SimpleDataBase/SimpleDataBase.cpp
@@ -1,9 +1,12 @@
 ﻿// Final assignment for the course "Fundamentals of C++: White Belt"
 // Done by Liubov Piliagina
 
-#include <iomanip>
+#include <cstdint>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 #include <cstdio>
+#include <stdexcept>
 #include <string>
 #include <set>
 #include <map>
@@ -38,7 +41,13 @@ class Date {
 public:
     Date(Year new_year, Month new_month, Day new_day) {
         year = new_year.year_value;
+        if (new_month.month_value > 12 || new_month.month_value < 1) {
+            throw logic_error("Month value is invalid: " + to_string(new_month.month_value));
+        }
         month = new_month.month_value;
+        if (new_day.day_value > 31 || new_day.day_value < 1) {
+            throw logic_error("Day value is invalid: " + to_string(new_day.day_value));
+        }
         day = new_day.day_value;
     }
     int GetYear() const {
@@ -57,11 +66,28 @@ private:
     int day;
 };
 
+bool operator < (const Date& lhs, const Date& rhs) {
+    if (lhs.GetYear() == rhs.GetYear()) {
+        if (lhs.GetMonth() == rhs.GetMonth()) {
+            return lhs.GetDay() < rhs.GetDay();
+        }
+        return lhs.GetMonth() < rhs.GetMonth();
+    }
+    return lhs.GetYear() < rhs.GetYear();
+};
+
+ostream& operator << (ostream& out, const Date& date) {
+    out << setw(4) << setfill('0') << date.GetYear() << "-" <<
+        setw(2) << setfill('0') << date.GetMonth() << "-" <<
+        setw(2) << setfill('0') << date.GetDay();
+    return out;
+}
+
 class Database {
 public:
     Database() {}
     Database(Date& date, string& event) {
-        AddEvent(date, event);
+        AddEvent(date, event);  //this constructor can be removed
     }
 
     void AddEvent(const Date& date, const string& event) {
@@ -91,23 +117,19 @@ public:
         cout << "Delete " << num_events << " events" << endl;
         return num_events;
     }
-    //redo - function find must return set
+    //function can return set
     void Find(const Date& date) const {
         if (database.count(date) == 1) {
             for (const auto& event : database.at(date)) {
-                cout << event << endl;
+                cout << event << endl;   //return database.at(date); return {};
             }
         }
     }
 
     void Print() const {
-        cout << setfill('0');
-        for (const auto& db : database){
-            // next line, after GetDay it is necessary to print ostream from Find
-            for (const auto& event : db.second) {
-                cout << setw(4) << db.first.GetYear() << "-" << setw(2)
-                    << db.first.GetMonth() << "-" << setw(2)
-                    << db.first.GetDay() << "-" << event << endl;
+        for (const auto& date_events : database){
+            for (const auto& event : date_events.second) {
+                cout << date_events.first << ": " << event << endl;
             }
         }
     }
@@ -115,48 +137,87 @@ private:
     map<Date, set<string>> database;
 };
 
-bool operator<(const Date& lhs, const Date& rhs) {
-    if (lhs.GetYear() == rhs.GetYear()) {
-        if (lhs.GetMonth() == rhs.GetMonth()) {
-            return lhs.GetDay() < rhs.GetDay();
-        }
-        return lhs.GetMonth() < rhs.GetMonth();
+void DetermineNextSymbolAndSkip(stringstream& ss, const string& date_str) {
+    if (ss.peek() != '-') {
+        throw logic_error("Invalid date format!" + date_str);
     }
-    return lhs.GetYear() < rhs.GetYear();
-};
+    ss.ignore(1);
+}
+Date ParseDate(const string& date_str) {
+    int year = 0, month = 0, day = 0;
 
-/*bool operator>(const Date& lhs, const Date& rhs) {
-    if (lhs.GetYear() == rhs.GetYear()) {
-        if (lhs.GetMonth() == rhs.GetMonth()) {
-            return lhs.GetDay() > rhs.GetDay();
-        }
-        return lhs.GetMonth() > rhs.GetMonth();
+    stringstream ss(date_str);
+    ss >> year;
+    DetermineNextSymbolAndSkip(ss, date_str);
+
+    ss >> month;
+    DetermineNextSymbolAndSkip(ss, date_str);
+
+    ss >> day;
+    if (!ss.eof()) {
+        throw logic_error("Invalid date format!" + date_str);
     }
-    return lhs.GetYear() > rhs.GetYear();
-}*/
-ostream& operator<<(ostream& out, Database& db) {
-    return out;
-};
+
+    return { Year{year}, Month{month}, Day{day} };
+}
 
 int main()
 {
-    Database db;
+    try {
+        Database db;
 
-    string command;
-    while (getline(cin, command)) {
-        //Считайте команды с потока ввода и обработайте каждую
+        string command_line;
+        while (getline(cin, command_line)) {
+            stringstream ss(command_line);
+        
+            string command;
+            ss >> command;
+            if (command == "Add") {
+                string date_str, event;
+                ss >> date_str >> event;
+
+                const Date date = ParseDate(date_str);
+
+                db.AddEvent(date, event);
+            }
+            else if (command == "Del") {
+                string date_str, event;
+                ss >> date_str;
+
+                if (!ss.eof()) {
+                    ss >> event;
+                }
+
+                const Date date = ParseDate(date_str);
+
+                if (!event.empty()) {
+                    db.DeleteEvent(date, event);
+                }
+                else {
+                    db.DeleteDate(date);
+                }
+            }
+            else if ( command == "Find") {
+                string date_str;
+                ss >> date_str;
+
+                const Date date = ParseDate(date_str);
+
+                db.Find(date);
+            }
+            else if (command == "Print") {
+                db.Print();
+            }
+            else if (command.empty()) {
+                break;
+            }
+            else {
+                throw logic_error("Unknown command: " + command);
+            }
+        }
     }
-
+    catch (const exception& error) {
+        cout << error.what() << endl;
+    }
     return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
